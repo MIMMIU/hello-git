2b4f38a87441f7318801583c8dec0d9b8700f018
Nayoung <kim_young5959@naver.com>
2023-05-16 16:06:51 +0900

    Update ABCD.java

--- src/com/sh/app/feature/ABCD.java
+++ src/com/sh/app/feature/ABCD.java
@@ -1,5 +1,17 @@
 package com.sh.app.feature;
 
 public class ABCD {
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		
+	}
+	
+	public ABCD() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
